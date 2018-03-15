8cd3606a9fc017275730ead882bf06137af48a91
Andrew <andrew.j.galle@gmail.com>
2018-03-15 12:05:40 -0400

    Cometic tweaks; it occurs to me I have other inventory management apps so I may redirect my efforts in the next commits to demonstrate different skill sets.

---
+++ FurnitureFun/Controllers/FileController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FurnitureFun.Data;
+
+namespace FurnitureFun.Controllers
+{
+    public class FileController : Controller
+    {
+        private Context db = new Context();
+        // GET: File
+        public ActionResult Index(int id)
+        {
+            var fileToRetrieve = db.Files.Find(id);
+            return File(fileToRetrieve.Content, fileToRetrieve.ContentType);
+
+            
+        }
+    }
+}
---
+++ FurnitureFun/Models/FileType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FurnitureFun.Models
+{
+    // This is for a new use case to associate images with new furniture uploads.  
+    // They will be stored as binary data in the database. 
+    public enum FileType
+    {
+      FurnitureImage = 1, Photo
+    } 
+}
