e019fd62818d83a0cc5f9ad7520fdb29605a82cf
Andrew <andrew.j.galle@gmail.com>
2018-03-20 19:33:09 -0400

    pointed to live database(external)

--- FurnitureFun/Data/Context.cs
+++ FurnitureFun/Data/Context.cs
@@ -16,8 +16,7 @@ namespace FurnitureFun.Data
         public DbSet<Order> Orders { get; set; } 
 
 
-        public Context()
-
+        public Context() : base ("Data Source=den1.mssql6.gear.host;Initial Catalog=furniturefun;Integrated Security=False;User ID=furniturefun;Password=Ou0Efw-bi0!T;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False") 
         {
             Database.SetInitializer(new DatabaseInitializer());
         }
